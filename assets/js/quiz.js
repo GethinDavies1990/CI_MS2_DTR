fffbd4e1937622e07a69677bd54ffcb934de9a72
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-08 21:22:13 +0000

    Feat: added all questions and answers to quiz game

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -108,72 +108,70 @@ const questions = [
         ]
     },
     {
-        question: 'England won the 2003 Rugby World Cup thanks to an iconic drop goal from Jonny Wilkinson. How many points did England score in the match?',
+        question: 'Did Cristiano Ronaldo score more goals or play more games for Real Madrid in all competitions?',
         answers: [
-            {text: '14', correct: false  },
-            {text: '33', correct: false },
-            {text: '20', correct: true },
-            {text: '7', correct: false }
+            {text: 'Scored more goals', correct: true },
+            {text: 'Played more games', correct: false }
 
         ]
     },
     {
-        question: 'England won the 2003 Rugby World Cup thanks to an iconic drop goal from Jonny Wilkinson. How many points did England score in the match?',
+        question: 'Who is England’s most capped football player?',
         answers: [
-            {text: '14', correct: false  },
-            {text: '33', correct: false },
-            {text: '20', correct: true },
-            {text: '7', correct: false }
+            {text: 'Wayne Rooney', correct: false  },
+            {text: 'David Beckham', correct: false },
+            {text: 'Peter Shilton', correct: true },
+            {text: 'Frank Lampard', correct: false }
 
         ]
     },
     {
-        question: 'England won the 2003 Rugby World Cup thanks to an iconic drop goal from Jonny Wilkinson. How many points did England score in the match?',
+        question: 'How many world titles has Phil ‘the Power’ Taylor won in darts?',
         answers: [
-            {text: '14', correct: false  },
-            {text: '33', correct: false },
-            {text: '20', correct: true },
-            {text: '7', correct: false }
+            {text: '24', correct: false  },
+            {text: '5', correct: false },
+            {text: '2', correct: false },
+            {text: '16', correct: true }
 
         ]
     },
     {
-        question: 'England won the 2003 Rugby World Cup thanks to an iconic drop goal from Jonny Wilkinson. How many points did England score in the match?',
+        question: 'Which England footballer was famously never given a yellow card?',
         answers: [
-            {text: '14', correct: false  },
-            {text: '33', correct: false },
-            {text: '20', correct: true },
-            {text: '7', correct: false }
+            {text: 'David Beckham', correct: false  },
+            {text: 'David Seamen', correct: false },
+            {text: 'Gary Lineker', correct: true },
+            {text: 'Ryan Giggs', correct: false }
 
         ]
     },
     {
-        question: 'England won the 2003 Rugby World Cup thanks to an iconic drop goal from Jonny Wilkinson. How many points did England score in the match?',
+        question: 'Who is Wales’s most capped rugby player?',
         answers: [
-            {text: '14', correct: false  },
-            {text: '33', correct: false },
-            {text: '20', correct: true },
-            {text: '7', correct: false }
+            {text: 'Alun Wyn Jones', correct: true  },
+            {text: 'Gethin Jenkins', correct: false },
+            {text: 'Shane Williams', correct: false },
+            {text: 'Mike Phillips', correct: false }
 
         ]
     },
     {
-        question: 'England won the 2003 Rugby World Cup thanks to an iconic drop goal from Jonny Wilkinson. How many points did England score in the match?',
+        question: 'Who is the Premier League’s all-time top scorer?',
         answers: [
-            {text: '14', correct: false  },
-            {text: '33', correct: false },
-            {text: '20', correct: true },
-            {text: '7', correct: false }
+            {text: 'David Beckham', correct: false  },
+            {text: 'Alan Shearer', correct: true },
+            {text: 'MIchael Carrick', correct: false },
+            {text: 'Paul Scholes', correct: false }
 
         ]
     },
     {
-        question: 'England won the 2003 Rugby World Cup thanks to an iconic drop goal from Jonny Wilkinson. How many points did England score in the match?',
+        question: 'Who is Britain’s most decorated Olympian?',
         answers: [
-            {text: '14', correct: false  },
-            {text: '33', correct: false },
-            {text: '20', correct: true },
-            {text: '7', correct: false }
+            {text: 'Paula Radcliffe', correct: false  },
+            {text: 'Jason Kenny', correct: false },
+            {text: 'Chris Hoy', correct: true },
+            {text: 'Bradley Wiggins', correct: true }
 
         ]
     },
