e0b186ac2178f697c49272515b1e33537ca4280b
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-16 17:59:11 +0000

    JS: added startGame & getNewQuestion function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,3 +3,27 @@ const choices = Array.from(document.querySelectorAll('.choice-text'))
 const progressText = document.querySelector('#progressText')
 const scoreText = document.querySelector('#score')
 const progressBarFull = document.querySelector('#progressBarFull')
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0 
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+  {
+  question: 'What is 2 + 2?',
+  choice1: '2',
+  choice2: '4',
+  choice3: '21',
+  choice4: '17',
+  answer: 2,
+},
+  question: 'What is 2 + 2?',
+  choice1: '2',
+  choice2: '4',
+  choice3: '21',
+  choice4: '17',
+  answer: 2,
+
+]
