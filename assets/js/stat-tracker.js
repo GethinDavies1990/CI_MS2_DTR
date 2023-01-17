6a72d47efc154b3dd25c96f425b9555d23c3ae6f
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-17 21:23:42 +0000

    API: changed league type

--- assets/js/stat-tracker.js
+++ assets/js/stat-tracker.js
@@ -1,18 +1,20 @@
-function topGoalScorer(cb) {
+function getData(cb) {
   var xhr = new XMLHttpRequest();
 
-  xhr.open("GET","https://apiv2.allsportsapi.com/football/?&met=Topscorers&leagueId=207&APIkey=4dc7334d95e72e4425145e1a6b33afaf7a41c8dd5765d736e958dfa001b13943");
+  xhr.open("GET", "https://apiv2.allsportsapi.com/football/?&met=Standings&leagueId=59&APIkey=4dc7334d95e72e4425145e1a6b33afaf7a41c8dd5765d736e958dfa001b13943")
   xhr.send();
 
-  xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      cb(JSON.parse(this.responseText));
-    }
+  xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          cb(JSON.parse(this.responseText));
+      }
   };
 }
 
-function writeToDocument(type) {
-	topGoalScorer(type, function() {
-		document.getElementById("")
-	})
+function printDataToConsole(data) {
+  console.log(data);
 }
+
+getData(printDataToConsole);
+
+document.getElementsByClassName('league-standings').innerHTML = `<h1>Hey</h1>`
