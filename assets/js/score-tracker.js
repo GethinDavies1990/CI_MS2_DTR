4e79696e02f264a40eff8669c1c13ca1df20dd40
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-17 20:53:23 +0000

    API fetch working

--- assets/js/score-tracker.js
+++ assets/js/score-tracker.js
@@ -1,19 +1,16 @@
-const data = null;
+function topGoalScorer(cb) {
+  var xhr = new XMLHttpRequest();
 
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+  xhr.open("GET","https://apiv2.allsportsapi.com/football/?&met=Topscorers&leagueId=207&APIkey=4dc7334d95e72e4425145e1a6b33afaf7a41c8dd5765d736e958dfa001b13943");
+  xhr.send();
 
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
-	}
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(JSON.parse(this.responseText));
+    }
+  };
+}
+
+topGoalScorer(function(topGoalScorer) {
+	console.log(topGoalScorer);
 });
-
-xhr.open("GET", "https://premier-league-standings1.p.rapidapi.com/?season=2022");
-xhr.setRequestHeader("X-RapidAPI-Key", "36a8401c4amsh48927ee11d65a19p11db18jsna2d76599bbd3");
-xhr.setRequestHeader("X-RapidAPI-Host", "premier-league-standings1.p.rapidapi.com");
-
-xhr.send(data);
-
-document.getElementById("score-tracker").innerHTML = `
-<h1>Premier League Standings</h1>`
