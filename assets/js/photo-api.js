5dc709af5088bd7edd84b8cba45912ef9b6b56fc
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-19 22:25:07 +0000

    API- stat tracker started

--- assets/js/photo-api.js
+++ assets/js/photo-api.js
@@ -1,3 +1,5 @@
+// Jquery/AJAX used to connect with unsplash API, youtube videos were consumed to achieve the result. 
+// the api, connects with my own unsplash account and a collection that was made
 $.getJSON(
   "https://api.unsplash.com/collections/6OtMro9rAxs/photos/?client_id=PFArrh-ILrPosvCml41PMDEjSlXGjJdpvjHPH76OLxA",
   function (data) {
@@ -9,12 +11,8 @@ $.getJSON(
       var name = value.user.name;
       var imageURL = value.urls.regular;
 
-    //   $('.name').text(name);
-    //   $('.image img').attr('src', imageURL);
-
-      $('.output').append('<h1 class="name">' + name + '</h1><div class="image"><img src="' + imageURL  + '"></div>');
+      $('.photo-api-container').append('<h4 class="photo-owner">' + name + '</h4><div class="image-api"><img src="' + imageURL  + '"></div>');
     });
   }
 );
 
-/* Access key PFArrh-ILrPosvCml41PMDEjSlXGjJdpvjHPH76OLxA */
--- assets/js/stat-test.js
+++ assets/js/stat-test.js
@@ -1,16 +1,20 @@
-function fetchLeague() {
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'b2f880e7ae15d7c6f8fa8a87b51b71aa',
             'X-RapidAPI-Host': 'v3.football.api-sports.io'
-        }
-    };
-    
-    fetch('https://v3.football.api-sports.io/standings?league=39&season=2022', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-        
 }
-fetchLeague()
+    }
+
+$.getJSON("https://v3.football.api-sports.io/standings?league=39&season=2022",
+    function (league) {
+      console.log(league);
+  
+      $.each(data, function (index, value) {
+        console.log(value);
+  
+        var name = value.user.name;
+        var imageURL = value.urls.regular;
+      });
+    }
+  );
