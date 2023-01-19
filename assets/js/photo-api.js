4a543df07d6958f41b6d68d1071bc23756e2a586
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-19 20:56:22 +0000

    JSON data being received

--- assets/js/photo-api.js
+++ assets/js/photo-api.js
@@ -1,14 +1,16 @@
-$.getJSON('https://api.unsplash.com/users/brianhaferkamp/photos/?client_id=PFArrh-ILrPosvCml41PMDEjSlXGjJdpvjHPH76OLxA', function(data){
+$.getJSON(
+  "https://api.unsplash.com/users/brianhaferkamp/photos/?client_id=PFArrh-ILrPosvCml41PMDEjSlXGjJdpvjHPH76OLxA",
+  function (data) {
     console.log(data);
-});
 
+    $.each(data, function (index, value) {
+      console.log(value);
 
-
-
-
-
-
+      var name = value.user.name;
+      var bio = value.user.bio;
+      var imageURL = value.urls.regular;
+    });
+  }
+);
 
 /* Access key PFArrh-ILrPosvCml41PMDEjSlXGjJdpvjHPH76OLxA */
-
-
