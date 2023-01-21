ba8c897ba33849b583b2e538a31188538afe67c4
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-21 14:21:50 +0000

    API: created a for loop to loop through each team name and output to the console.

--- assets/js/stat-test.js
+++ assets/js/stat-test.js
@@ -1,44 +1,18 @@
-//     const options = {
-// //         method: 'GET',
-// //         headers: {
-// //             'X-RapidAPI-Key': 'b2f880e7ae15d7c6f8fa8a87b51b71aa',
-// //             'X-RapidAPI-Host': 'v3.football.api-sports.io'
-// // }
-//     }
-
-// $.getJSON("https://v3.football.api-sports.io/standings?league=39&season=2022",
-//     function (league) {
-//       console.log(league);
-  
-//       $.each(data, function (index, value) {
-//         console.log(value);
-  
-//         var name = value.user.name;
-//         var imageURL = value.urls.regular;
-//       });
-//     }
-//   );
-
 var settings = {
-  "url": "https://v3.football.api-sports.io/standings?league=39&season=2022",
-  "method": "GET",
-  "timeout": 0,
-  "headers": {
+  url: "https://v3.football.api-sports.io/standings?league=39&season=2022",
+  method: "GET",
+  timeout: 0,
+  headers: {
     "x-rapidapi-key": "b2f880e7ae15d7c6f8fa8a87b51b71aa",
-    "x-rapidapi-host": "v3.football.api-sports.io"
+    "x-rapidapi-host": "v3.football.api-sports.io",
   },
 };
 
-$.ajax(settings).done(function (league) {
-  console.log(league);
-
-  $.each(league, function (index, value) {
-        console.log(value);
-  
-        // var name = value.user.name;
-        // var imageURL = value.urls.regular;
-  
-
-
-      })
-    });
+// Gets the team names using the getJSON method in jQuery, loops through each team and outputs to the console. 
+$.ajax(settings).done(function getTeamName (league) {
+    var standingsLength = league.response[0].league.standings[0].length;
+    for (var i = 0; i < standingsLength; i++) {
+      var teamName = league.response[0].league.standings[0][i].team.name;
+      console.log(teamName);
+    }
+  });
