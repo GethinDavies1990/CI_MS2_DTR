277be4efba23708949c22d6b8cd36cf07bc613cc
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-23 17:07:09 +0000

    API: top goal scorer, amended the league year, wrong year was displaying

--- assets/js/top-scorers.js
+++ assets/js/top-scorers.js
@@ -1,6 +1,6 @@
 // API used to bring in the latest standings for the top goal scorers in the premier league, the api is limited to 100 requests a day
 var settings2 = {
-  url: "https://v3.football.api-sports.io/players/topscorers?season=2020&league=39",
+  url: "https://v3.football.api-sports.io/players/topscorers?season=2022&league=39",
   method: "GET",
   timeout: 0,
   headers: {
