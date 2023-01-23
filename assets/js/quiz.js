8f2a86ba5c136d0efe613d89a0e66bbc0da5416c
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-23 19:36:34 +0000

    quiz, questions added

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
@@ -6,9 +7,12 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
-    console.log('Started')
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -17,6 +21,7 @@ function startGame() {
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
@@ -34,20 +39,130 @@ function showQuestion(question) {
     })
 }
 
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
 
-function selectAnswer() {
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+    nextButton.classList.remove('hide')
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('body-correct')
+    } else {
+        element.classList.add('body-wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('body-correct')
+    element.classList.remove('body-wrong')
 }
 
 const questions = [
     {
-        question: 'What is 2 + 2?',
+        question: 'What sport is described as "the beautiful game"?',
         answers: [
-            {text: '4', correct: true},
-            {text: '6', correct: false},
-            {text: '10', correct: false},
-            {text: '1', correct: false},
-
+            {text: 'Rugby', correct: false},
+            {text: 'Football', correct: true},
+            {text: 'Tennis', correct: false},
+            {text: 'Golf', correct: false},
         ]
-    }
+    },
+    {
+        question: 'Jurgen Klopp began his managerial career at which German club?',
+        answers: [
+        {text: 'Borussia Dortmund', correct: false},
+        {text: 'VFL Wolfsburg', correct: false},
+        {text: 'Bayern Munich', correct: false},
+        {text: 'Mainz 05 ', correct: true}
+    ]
+},
+    { 
+        question: "Which country's rugby team call themselves the springboks?",
+    answers: [
+        {text: 'Wales', correct: false},
+        {text: 'South Africa', correct: false},
+        {text: 'Germany', correct: false},
+        {text: 'Australia', correct: true}
+    ]
+},
+    {
+        question: 'The Chicago Cubs and Boston Red Sox play which sport?',
+        answers: [
+            {text: 'Basketball', correct: false},
+            {text: 'American Football', correct: false},
+            {text: 'Croquet', correct: false},
+            {text: 'Baseball', correct: true}
+        ]},
+    { 
+        question: "Who is the Premier League's all-time top scorer?",
+    answers: [
+        {text: 'Alan Shearer', correct: true},
+        {text: 'Wayne Rooney', correct: false},
+        {text: 'David Beckham', correct: false},
+        {text: 'Didier Dogba', correct: false}
+    ]
+},
+    {  
+        question: 'In which sport do teams compete to win the Stanley Cup?',
+    answers: [
+        {text: 'Rugby', correct: false},
+        {text: 'Ice Hockey (NHL)', correct: true},
+        {text: 'Basketball (NBA)', correct: false},
+        {text: 'Golf', correct: false}
+    ]
+},
+    {
+        question: 'How many MMA fights has UFC star Conor McGregor lost during his career?',
+    answers: [
+        {text: 'Six', correct: true},
+        {text: 'Ten', correct: false},
+        {text: 'Zero', correct: false},
+        {text: 'Three', correct: false}
+    ]
+},
+    { 
+        question: 'Who has won more grand slam tennis titles: Serena Williams or Roger Federer?',
+    answers: [
+        {text: 'Serena Williams', correct: true},
+        {text: 'Roger Federer', correct: false}
+    ]
+},
+    { 
+        question: 'The Los Angeles Lakers and New York Knicks play which sport?',
+    answers: [
+        {text: 'Baseball', correct: false},
+        {text: 'American Football (NFL)', correct: false},
+        {text: 'Ice Hockey (NHL)', correct: false},
+        {text: 'Basketball (NBA)', correct: true}
+    ]
+},
+    { 
+        question: 'Who won more caps for England: Wayne Rooney or David Beckham?',
+    answers: [
+        {text: 'David Beckham', correct: false},
+        {text: 'Wayne Rooney', correct: true}
+        
+    ]}
+       
 ]
