dcb2f83ac8affa0618b839fc6207c150dcd322a4
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-29 20:26:22 +0000

    quiz: added showQuestions function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -18,4 +18,29 @@ exitBtn.onclick = () => {
 continueBtn.onclick = () => {
     infoBox.classList.remove("activeInfo"); // Removes the info box
     quizBox.classList.add("activeQuiz"); // Displays the quiz box
+    showQuestions(1);
+}
+
+let queCount = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+
+// Next Button Function
+nextBtn.onclick = () => {
+    queCount++
+    showQuestions(queCount);
+}
+
+// Gets questions and options from the array
+
+function showQuestions(index) {
+    const queText = document.querySelector("que-text");
+    const optionList = document.querySelector("option-list");
+    let queTag = '<span>'+ questions[index].question +'</span>';
+    let optionTag = '<div class="option">'+ questions[0].options[0] +'<span></span></div>'
+                    + '<div class="option">'+ questions[1].options[1] +'<span></span></div>'
+                    + '<div class="option">'+ questions[2].options[2] +'<span></span></div>'
+                    + '<div class="option">'+ questions[3].options[3] +'<span></span></div>'
+    queText.innerHTML = queTag;
+    optionList.innerHTML = optionTag;
 }
