b6a3f0df63e37ee95f30aa3a930d1f170b04cabc
GethinDavies1990 <gethindavies1990@gmail.com>
2023-01-30 21:02:42 +0000

    quiz wroking, need to further style

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,109 +1,89 @@
 let questions = [
-    {
-        numb: 1,
-        question: 'What sport is described as "the beautiful game"?',
-        answer: 'Football',
-        options: [
-             'Rugby',
-             'Football',
-             'Tennis', 
-             'Golf'
-        ]
-    },
-    {
-        numb: 2,
-        question: 'Jurgen Klopp began his managerial career at which German club?',
-        answer: 'Mainz 05',
-        options: [
-             'Borussia Dortmund',
-             'VFL Wolfsburg',
-             'Bayern Munich', 
-             'Mainz 05'
-        ]
-    },
-    {
-        numb: 3,
-        question: "Which country's rugby team call themselves the springboks?",
-        answer: 'Australia',
-        options: [
-            'Wales',
-            'South Africa',
-            'Germany',
-            'Australia'
-        ]
-    },
-    {
-        numb: 4,
-        question: 'The Chicago Cubs and Boston Red Sox play which sport?',
-        answer: 'Baseball',
-        options: [
-            'Basketball',
-            'American Football',
-            'Croquet',
-            'Baseball'
-        ]
-    },
-    {
-        numb: 5,
-        question: "Who is the Premier League's all-time top scorer?",
-        answer: 'Alan Shearer',
-        options: [
-            'Alan Shearer',
-            'Wayne Rooney',
-            'David Beckham',
-            'Didier Dogba'
-        ]
-    },     
-    {
-        numb: 6,
-        question: "In which sport do teams compete to win the Stanley Cup?",
-        answer: 'Ice Hockey (NHL)',
-        options: [
-            'Rugby',
-             'Ice Hockey (NHL)',
-             'Basketball (NBA)',
-             'Golf'
-        ]
-    },
-    {
-        numb: 7,
-        question: "How many MMA fights has UFC star Conor McGregor lost during his career?",
-        answer: 'Six',
-        options: [
-            'Six',
-            'Ten', 
-            'Zero', 
-            'Three',
-        ]
-    },
-    {
-        numb: 8,
-        question: "Who has won more grand slam tennis titles: Serena Williams or Roger Federer?",
-        answer: 'Serena Williams',
-        options: [
-            'Serena Williams',
-            'Roger Federer'
-        ]
-    }, 
-    {
-    numb: 9,
-        question: "The Los Angeles Lakers and New York Knicks play which sport?",
-        answer: 'Basketball (NBA)',
-        options: [
-            'Baseball',
-             'American Football (NFL)',
-             'Ice Hockey (NHL)',
-             'Basketball (NBA)'       
+        { 
+            question: 'What sport is described as "the beautiful game"?',
+                answers: [
+                    { text: 'Rugby', correct: false},
+                    { text: 'Football', correct: true},
+                    { text: 'Tennis', correct: false},
+                    { text: 'Golf', correct: false},
             ]
-    }, 
-    {
-    numb: 10,
-        question: "Who won more caps for England: Wayne Rooney or David Beckham?",
-        answer: 'Wayne Rooney',
-        options: [
-            'David Beckham',
-            'Wayne Rooney'
-        ]
-    }       
-       
-]
+        },
+        {
+            question: 'Jurgen Klopp began his managerial career at which German club?',
+                answers: [
+                    {text: 'Borussia Dortmund', correct: false},
+                    {text: 'VFL Wolfsburg', correct: false},
+                    {text: 'Bayern Munich', correct: false},
+                    {text: 'Mainz 05 ', correct: true}
+            ]
+        },
+        { 
+                question: "Which country's rugby team call themselves the springboks?",
+                answers: [
+                    {text: 'Wales', correct: false},
+                    {text: 'South Africa', correct: false},
+                    {text: 'Germany', correct: false},
+                    {text: 'Australia', correct: true}
+            ]
+        },
+        {
+                question: 'The Chicago Cubs and Boston Red Sox play which sport?',
+                answers: [
+                    {text: 'Basketball', correct: false},
+                    {text: 'American Football', correct: false},
+                    {text: 'Croquet', correct: false},
+                    {text: 'Baseball', correct: true}
+                ]
+        },
+        { 
+                question: "Who is the Premier League's all-time top scorer?",
+                answers: [
+                    {text: 'Alan Shearer', correct: true},
+                    {text: 'Wayne Rooney', correct: false},
+                    {text: 'David Beckham', correct: false},
+                    {text: 'Didier Dogba', correct: false}
+            ]
+        },
+        {  
+                question: 'In which sport do teams compete to win the Stanley Cup?',
+                answers: [
+                    {text: 'Rugby', correct: false},
+                    {text: 'Ice Hockey (NHL)', correct: true},
+                    {text: 'Basketball (NBA)', correct: false},
+                    {text: 'Golf', correct: false}
+            ]
+        },
+            {
+                question: 'How many MMA fights has UFC star Conor McGregor lost during his career?',
+                answers: [
+                    {text: 'Six', correct: true},
+                    {text: 'Ten', correct: false},
+                    {text: 'Zero', correct: false},
+                    {text: 'Three', correct: false}
+            ]
+        },
+            { 
+                question: 'Who has won more grand slam tennis titles: Serena Williams or Roger Federer?',
+                answers: [
+                    {text: 'Serena Williams', correct: true},
+                    {text: 'Roger Federer', correct: false}
+            ]
+        },
+            { 
+                question: 'The Los Angeles Lakers and New York Knicks play which sport?',
+                answers: [
+                    {text: 'Baseball', correct: false},
+                    {text: 'American Football (NFL)', correct: false},
+                    {text: 'Ice Hockey (NHL)', correct: false},
+                    {text: 'Basketball (NBA)', correct: true}
+            ]
+        },
+            { 
+                question: 'Who won more caps for England: Wayne Rooney or David Beckham?',
+                answers: [
+                    {text: 'David Beckham', correct: false},
+                    {text: 'Wayne Rooney', correct: true}
+                
+            ]}
+        
+    ]
