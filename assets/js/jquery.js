3dbcdf85d6f9cd292b0094365d709144aec2cd71
GethinDavies1990 <gethindavies1990@gmail.com>
2023-02-06 10:59:24 +0000

    added h1 tag to rules

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,6 +1,6 @@
 // Adds the toggle effect to the Rules of the quiz container
 $(function() {
-    $("#quiz-rules-container h2").on("click", function() {
+    $("#quiz-rules-container h1").on("click", function() {
       $(".quiz-rules").slideToggle();
     });
   });
