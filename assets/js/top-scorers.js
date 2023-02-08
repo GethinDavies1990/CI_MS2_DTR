aa4799bc51e072adb508efb74c22550a74c7a6fd
GethinDavies1990 <gethindavies1990@gmail.com>
2023-02-08 22:17:09 +0000

    fix: code clean top-scorer

--- assets/js/top-scorers.js
+++ assets/js/top-scorers.js
@@ -1,5 +1,8 @@
-// API used to bring in the latest standings for the top goal scorers in the premier league, the api is limited to 100 requests a day
-// Followed the API-Sports documentation to see how to achieve the endpoint I wanted to retrieve, the API was purchased through rapid api on freemium package
+/**
+ * API used to bring in the latest standings for the premier league, the api is limited to 100 requests a day
+ * settings, are the parameters required to fetch the data from the API-football
+ * followed the API documentation to see how to implement the settings into my code
+ */
 var settings2 = {
   url: "https://v3.football.api-sports.io/players/topscorers?season=2022&league=39",
   method: "GET",
@@ -10,35 +13,36 @@ var settings2 = {
   },
 };
 
-// I found a youtube video of how a developer used an api to display upsplash images in their website, 
-// I used the same approach how to extract the data needed, and followed that same princicpal below for each type of data i wanted to retrive
-// Gets the top goal scorers of the premier league using the getJSON method in jQuery
+/**
+ * GetJSON method was used to fetch the data from the API-Sports
+ * I followed the documentation on the API-sports website to see how to fetch the data correctly,
+ * Gets the top goal scorers of the premier league using the getJSON method in jQuery
+ * and also checked back on the API module on the code institutes learning platform.
+ * Line 30 loops through player and outputs their photo
+ * Line 31 loops through player and outputs their name
+ * Line 32 loops through player and outputs their clubs logo
+ * Line 33 loops through player and outputs their goal tally
+ * Line 34 loops through player and outputs their assists tally
+ */
 $.ajax(settings2).done(function scorereData(topScorer) {
   var players = topScorer.response;
   for (var i = 0; i < players.length; i++) {
-    
-    // loops through player and outputs their photo
     var playerPhoto = players[i].player.photo;
-    // console.log(playerPhoto);
-
-    // loops through player and outputs their name
-    var playerName = players[i].player.name;
-    // console.log(playerName);
-
-    // loops through player and outputs their clubs logo
+    var playerName = players[i].player.name; 
     var teamLogo = players[i].statistics[0].team.logo;
-    // console.log(clubName);
-
-    // loops through player and outputs their goal tally
     var playerGoal = players[i].statistics[0].goals.total;
-    // console.log(playerGoal);
-
-    // loops through player and outputs their goal tally
     var playerAssists = players[i].statistics[0].goals.assists;
-    // console.log(playerAssists);
 
-
-    $("#scorer-table tbody").append(`<tr><td><img class='player-photo' alt='${playerName} profile picture' src='${playerPhoto}'> ${playerName}</td><td><img class='team-logo' alt='${teamLogo} crest badge' src='${teamLogo}'></td><td>${playerGoal}</td><td>${playerAssists}</td></tr>`);
+    /**
+     * inputs top goal scorer data into a HTML table using template literals,
+     *  the class of 'mob-hide' was added to less important data to help improve responsiveness on mobile
+     */
+    $("#scorer-table tbody").append(`<tr>
+    <td><img class='player-photo' alt='${playerName} profile picture' src='${playerPhoto}'> ${playerName}</td>
+    <td><img class='team-logo' alt='${teamLogo} crest badge' src='${teamLogo}'></td>
+    <td>${playerGoal}</td>
+    <td>${playerAssists}</td>
+    </tr>`);
 
 
   }
