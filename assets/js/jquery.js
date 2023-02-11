43f739196c5da600a090303fe434d2d1effd01b8
GethinDavies1990 <gethindavies1990@gmail.com>
2023-02-11 15:51:10 +0000

    fix: added index.js

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -5,19 +5,5 @@ $(function () {
   });
 });
 
-/* global Typed */
-// Typing effect in homepage
-var typeEffect = {};
 
-    $(document).ready(function() {
-      typeEffect.typed = new Typed(".typing-effect", {
-        strings: [
-          "Welcome to LoveSports!",
-          "The No.1 place to keep up to date with everything<br>sport's related",
-        ],
-        typeSpeed: 70,
-        backSpeed: 70,
-        loop: false,
-      });
-    });
 
